e43101530b54a9edbc97552ef0bc2e43997d9a8c
Alain D'Ettorre <alain.det@gmail.com>
2021-05-21 09:20:50 +0200

    WIP: Add config

--- slim/src/bootstrap.php
+++ slim/src/bootstrap.php
@@ -4,6 +4,9 @@ use Slim\Factory\AppFactory;
 
 use App\Core\Exceptions\ExceptionHandler;
 
+// Declare .env vars as constants
+$config = require __DIR__ . './config.php';
+
 require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
